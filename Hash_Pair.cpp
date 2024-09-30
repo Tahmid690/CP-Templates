9984c71a80b293fee42f8e3eed092f793eb9e027
Tahmid <45348952+Tahmid690@users.noreply.github.com>
2024-09-30 18:27:57 +0600

    Create Hash_Pair.cpp

---
+++ Hash_Pair.cpp
@@ -0,0 +1,14 @@
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        // Hash the first element
+        size_t hash1 = hash<T1>{}(p.first);
+        // Hash the second element
+        size_t hash2 = hash<T2>{}(p.second);
+        // Combine the two hash values
+        return hash1
+               ^ (hash2 + 0x9e3779b9 + (hash1 << 6)
+                  + (hash1 >> 2));
+    }
+};
