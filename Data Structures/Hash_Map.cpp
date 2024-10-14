d999684edb6508845e0a68d4ceacb03490fe38cf
Tahmid <45348952+Tahmid690@users.noreply.github.com>
2024-10-14 16:18:16 +0600

    Update Hash_Map.cpp

--- Data Structures/Hash_Map.cpp
+++ Data Structures/Hash_Map.cpp
@@ -1,3 +1,8 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+
 typedef cc_hash_table<ll, ll, hash<ll>> ht;
 
 //Use ht as unordered_map
@@ -7,6 +12,22 @@ struct chash {
     int operator()(int x) const { return x ^ RANDOM; }
 };
 gp_hash_table<int, int, chash> table;
+
+
+struct custom_hash {
+  static uint64_t splitmix64(uint64_t x) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
+
+gp_hash_table<int, int, custom_hash> mp;
 
 //For Pair
 
