73eeda6596bd719bb5dc04d728280d9a0cb3ba49
Tahmid <45348952+Tahmid690@users.noreply.github.com>
2024-10-15 19:55:26 +0600

    Update Sparse_Table.cpp

--- Data Structures/Sparse_Table.cpp
+++ Data Structures/Sparse_Table.cpp
@@ -1,12 +1,9 @@
 #define N 200005
-int sparse[N][25],logtable[N];
-void build_sparse(int ara[],int n){
-    logtable[1]=0;
-    for(int i=2;i<N;i++) logtable[i]=logtable[i/2]+1;
-    
-    for(int i=0;i<n;i++) sparse[i][0]=ara[i];
-    for(int j=1;j<=25;j++){
-        for(int i=0;i+(1<<j)<=n;i++){
+int sparse[N][25],a[N];
+void build_sparse(int n){
+    for(int i=1;i<=n;i++) sparse[i][0]=a[i];
+    for(int j=1;j<25;j++){
+        for(int i=1;i+(1<<j)-1<=n;i++){
             sparse[i][j]=sparse[i][j-1]+sparse[i+(1<<(j-1))][j-1];
         }
     }
@@ -22,8 +19,8 @@ int query_sum(int l,int r){
     return sum;
 }
 
-int min_query(int l,int r){
-    int j=logtable[r-l+1];
+int query(int l,int r){
+    int j=31 - __builtin_clz(r-l+1);
     int mini=min(sparse[l][j],sparse[r-(1<<j)+1][j]);
     return mini;
 }
